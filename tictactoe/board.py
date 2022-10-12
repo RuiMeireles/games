92639489b072268e846d8baa3fe5b6203b987e2c
Rui Meireles <ruivitormeireles@gmail.com>
2022-10-12 22:31:02 +0200

    Tests for all board methods

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -11,21 +11,24 @@ def empty_grid() -> Grid:
 class Board:
     grid: Grid = field(default_factory=empty_grid)
 
-    def from_str(self, s: str):
+    @classmethod
+    def from_str(cls, s: str):
+        str_to_symbol = {symbol.value: symbol for symbol in Symbol}
+        error_msg = "Not a valid string to describe a Board"
+
+        board = cls()
         rows = s.strip("\n").split("\n")
         if len(rows) != GRID_SIZE:
-            raise ValueError("Not a valid string to describe a Board")
+            raise ValueError(error_msg)
         for row, row_str in enumerate(rows):
             if len(row_str) != GRID_SIZE:
-                raise ValueError("Not a valid string to describe a Board")
+                raise ValueError(error_msg)
             for col, char in enumerate(row_str):
-                self.grid[Position(row, col)] = Symbol._value2member_map_[char]
-        return self
-
-    def place(self, position: Position, symbol: Symbol) -> None:
-        if self.grid[position] != Symbol.EMPTY:
-            raise ValueError("Can't place a symbol on a non-empty grid cell.")
-        self.grid[position] = symbol
+                try:
+                    board.grid[Position(row, col)] = str_to_symbol[char]
+                except KeyError:
+                    raise ValueError(error_msg)
+        return board
 
     def __str__(self):
         s = ""
@@ -34,3 +37,8 @@ class Board:
                 s += self.grid[Position(row, col)].value
             s += "\n"
         return s.strip("\n")
+
+    def place(self, position: Position, symbol: Symbol) -> None:
+        if self.grid[position] != Symbol.EMPTY:
+            raise ValueError("Can't place a symbol on a non-empty grid cell.")
+        self.grid[position] = symbol
--- tictactoe/tests/test_board.py
+++ tictactoe/tests/test_board.py
@@ -1,14 +1,36 @@
 import unittest
 
 from tictactoe.board import Board
+from tictactoe.common import Position, Symbol
 
 
 class TestBoardMethods(unittest.TestCase):
     def test_new_board_has_empty_grid(self):
-        self.assertEqual(str(Board()), "   \n   \n   ")
+        str_empty_board = "   \n   \n   "
+        self.assertEqual(str(Board()), str_empty_board)
 
     def test_new_board_from_str(self):
-        self.assertEqual(str(Board().from_str("X  \n   \n   ")), "X  \n   \n   ")
+        str_board = "X  \n   \n   "
+        self.assertEqual(str(Board.from_str(str_board)), str_board)
+
+    def test_new_board_from_invalid_str_fails(self):
+        with self.assertRaises(ValueError):
+            Board.from_str(" X \n O \nX")
+        with self.assertRaises(ValueError):
+            Board.from_str(" X \n O ")
+        with self.assertRaises(ValueError):
+            Board.from_str(" R \n O \n X ")
+
+    def test_board_place_symbol_succeeds(self):
+        board = Board()
+        board.place(Position(1, 1), Symbol.X)
+        str_board = "   \n X \n   "
+        self.assertEqual(str(board), str_board)
+
+    def test_board_place_symbol_fails(self):
+        board = Board.from_str("   \n X \n   ")
+        with self.assertRaises(ValueError):
+            board.place(Position(1, 1), Symbol.O)
 
 
 if __name__ == "__main__":
